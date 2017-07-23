ba4e6a1b0684f529b5a2b83cde3b7d4cf47be7bb
ryanolee <landgraabiv@gmail.com>
2017-07-23 23:03:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"/>
+
 <?php
+//First we require our uploader class
 require("/uploader.php");
-$upload=new \uploader\upload("/uploads/u/",array("image/jpeg"),"test1","next.php");
+//Then we create a new instance of the uploader class
+$upload=new \uploader\upload("/uploads/",array("image/png","image/jpeg"),"test1","next.php");
+//finally we get the html for that segment
 echo $upload->get_html_segment();
-$upload=new \uploader\upload("/uploads/u/",array("image/jpeg"),"test2","next.php");
-echo $upload->get_html_segment();
-$upload=new \uploader\upload("/uploads/u/",array("image/jpeg"),"test3","next.php");
-echo $upload->get_html_segment();
-$upload=new \uploader\upload("/uploads/u/",array("image/jpeg"),"test4","next.php");
-echo $upload->get_html_segment();
-$upload=new \uploader\upload("/uploads/u/",array("image/jpeg"),"test5","next.php");
-echo $upload->get_html_segment();
+//this is true for multiple uploads
+$upload2=new \uploader\upload("/uploads/",array("audio/mpeg"),"test2","next.php");
+//Fetching the html segment for this upload can be done at any time
+echo $upload2->get_html_segment();
 ?>
