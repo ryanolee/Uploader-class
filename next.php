8eef475e84d973b408064f5453cbf31d15f8f537
ryanolee <landgraabiv@gmail.com>
2017-07-23 23:04:43 +0100

    Update next.php

--- next.php
+++ next.php
@@ -1,4 +1,6 @@
 <?php
 require("/uploader.php");
+//Headers cannot be sent before this point
+//Simply call the handle upload function and any uploads will be automaticly verified (The script will exit on it bieng called).
 \uploader\handle_upload();
 ?>
