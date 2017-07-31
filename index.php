d6b04f324909fb1bde48ef8e04e43ec765f70120
ryanolee <landgraabiv@gmail.com>
2017-07-31 01:11:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 //First we require our uploader class
 require("/uploader.php");
 //Then we create a new instance of the uploader class
-$upload=new \uploader\upload("/uploads/",array("image/png","image/jpeg"),"test1","next.php");
+$upload=new \uploader\upload(__DIR__."/uploads/",array("image/png","image/jpeg"),"test1","next.php");
 //finally we get the html for that segment
 echo $upload->get_html_segment();
 //this is true for multiple uploads
-$upload2=new \uploader\upload("/uploads/",array("audio/mpeg"),"test2","next.php");
+$upload2=new \uploader\upload(__DIR__."/uploads/",array("audio/mpeg"),"test2","next.php");
 //Fetching the html segment for this upload can be done at any time
 echo $upload2->get_html_segment();
 ?>
